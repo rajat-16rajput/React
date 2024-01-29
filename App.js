6c54b391a7029d96a131525d3eedbc78de82e268
rajat-16rajput <67589444+rajat-16rajput@users.noreply.github.com>
2024-01-29 19:43:32 +0530

    Chapter 3 Element, Component Composition

--- App.js
+++ App.js
@@ -1,24 +1,40 @@
-// const header = React.createElement(
-//   "div",
-//   { id: "temp" },
-//   React.createElement("div", {}, [
-//     React.createElement("h1", {}, "hello world"),
-//     React.createElement("h2", {}, "hello world"),
-//   ])
-// );
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Title = () => {
-  return <h2>Hellu world from element</h2>;
+// ● Create a Nested header Element using React.createElement(h1,h2,h3 inside a
+//   div with class “title”)
+//   ○ Create the same element using JSX
+//   ○ Create a functional component of the same with JSX
+//   ○ Pass attributes into the tag in JSX
+//   ○ Composition of Component(Add a component inside another)
+//   ○ {TitleComponent} vs {<TitleComponent/>} vs
+//   {<TitleComponent></TitleComponent>} in JSX
+
+// const answer = React.createElement("div", {}, [
+//   React.createElement("h1", {}, "H1 space"),
+//   React.createElement("h2", {}, "H2 space"),
+//   React.createElement("h2", {}, "H3 space"),
+// ]);
+
+const headerElement = (
+  <div>
+    <h1>H1 space</h1>
+    <h2>H2 space</h2>
+    <h3>H3 space</h3>
+  </div>
+);
+
+const Temp = () => {
+  return <h2>HE MAn</h2>;
 };
-const HeadingComponent = () => {
+
+const FunctionalComponent = () => {
   return (
-    <div>
-      {Title()}
-      <h1>Hello World inside functional component </h1>
-    </div>
+    <>
+      <Temp></Temp>
+      {headerElement}
+    </>
   );
 };
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<FunctionalComponent />);
