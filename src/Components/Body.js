a62dcde640f94c7f93779b9762eee862f18ab7d9
rajat-16rajput <67589444+rajat-16rajput@users.noreply.github.com>
2024-02-07 06:48:21 +0530

    Add search functionality. Chapter 6

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -4,7 +4,8 @@ import Shimmer from "./Shimmer";
 import Card from "./Card";
 const Body = () => {
   const [listofHotels, setListOfHotels] = useState([]);
-
+  const [searchText, setSearchText] = useState("");
+  const [searchList, setSearchList] = useState([]);
   useEffect(() => {
     console.log("use Effect called");
     fetchData();
@@ -15,30 +16,56 @@ const Body = () => {
     const json = await data.json();
 
     setListOfHotels(
-      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+      json?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
-    // console.log(listofHotels);
+    setSearchList(
+      json?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    console.log(listofHotels);
   };
 
   return (
     <>
       <div>
+        <input
+          type="text"
+          className="search-box"
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value); //component will re render with every letter typed
+          }}
+        ></input>
+        <button
+          className="search-btn"
+          onClick={() => {
+            //It will filter from the original list of restaurants if matching case found then it will update the search list from the list of hotels
+            const filteredRestaurant = listofHotels.filter((element) =>
+              element.info.name.toLowerCase().includes(searchText.toLowerCase())
+            );
+            setSearchList(filteredRestaurant);
+          }}
+        >
+          Search
+        </button>
+
         <button
           className="filter-button"
           onClick={() => {
             const filteredList = listofHotels.filter(
               (element) => element.info.avgRating > 4
             );
-            console.log(listofHotels);
-            console.log(filteredList);
-            setListOfHotels(filteredList);
+            {
+              /* displaying the searchlist */
+            }
+            setSearchList(filteredList);
           }}
         >
           top restauants
         </button>
       </div>
       <div className="card-container">
-        {listofHotels.map((resObj) => (
+        {/* displaying the searchlist */}
+        {searchList.map((resObj) => (
           <Card key={resObj.id} resObj={resObj} />
         ))}
       </div>
